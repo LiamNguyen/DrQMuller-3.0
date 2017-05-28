ecb98a70fbc5a52e548a217071afd0fee29bf83f
Liam Nguyen <phuc.nguyen2@metropolia.fi>
2017-05-28 18:16:16 +0300

    Compromise SwiftLint for GsMessage class

--- DrQMuller/GSMessage.swift
+++ DrQMuller/GSMessage.swift
@@ -293,7 +293,7 @@ extension UIView {
 private var installedMessageKey = ""
 private var uninstallMessageKey = ""
 
-private func GS_GCDAfter(_ delay: Double, closure:@escaping ()->Void) {
+private func GS_GCDAfter(_ delay: Double, closure:@escaping () -> Void) {
     DispatchQueue.main.asyncAfter(
         deadline: DispatchTime.now() + Double(Int64(delay * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC), execute: closure)
 }
