e819caa5ba05a89369fc07cf6360661b59d6e365
Liam Nguyen <phuc.nguyen2@metropolia.fi>
2017-06-01 11:20:00 +0300

    Add validation helper

---
+++ DrQMuller/ValidationHelper.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+class ValidationHelper {
+    fileprivate static let rules: [String: String] = [
+        "username": "^[A-Za-z0-9\\-\\_]{8,25}$",
+        "password": "^[A-Za-z0-9\\-\\_]{8,30}$"
+    ]
+
+    static func validate(scheme: [String: String]) -> [(key: String, errorCode: String)] {
+		let errors = scheme
+			.map { (key, value) -> (key: String, errorCode: String) in
+				if value.isEmpty {
+					return (key: key, errorCode: ErrorCode.empty.rawValue)
+				}
+				if value.range(of: rules[key] ?? "", options: .regularExpression) == nil {
+					return (key: key, errorCode: ErrorCode.patternFail.rawValue)
+				}
+				return (key: "", errorCode: "")
+			}
+			.filter { (key: String, errorCode: String) -> Bool in
+				return !key.isEmpty && !errorCode.isEmpty
+		}
+		return errors.isEmpty ? [(key: String, errorCode: String)]() : errors
+    }
+
+    fileprivate enum ErrorCode: String {
+        case patternFail
+        case empty
+    }
+}
