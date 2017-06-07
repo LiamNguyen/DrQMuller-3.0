a45989e1225478390b62bf2b25233a38f44f9b1c
Liam Nguyen <phuc.nguyen2@metropolia.fi>
2017-06-07 14:50:18 +0300

    return success or fail for save and remove from User Defaults

--- DrQMuller/UserDefaultsService.swift
+++ DrQMuller/UserDefaultsService.swift
@@ -1,16 +1,18 @@
 import Foundation
 
 class UserDefaultsService {
-    static func save(forKey: Keys, data: Any) {
+    static func save(forKey: Keys, data: Any) -> Bool {
         UserDefaults.standard.set(data, forKey: forKey.rawValue)
+        return UserDefaultsService.get(forKey: forKey) != nil
     }
 
     static func get(forKey: Keys) -> Any? {
         return UserDefaults.standard.value(forKey: forKey.rawValue)
     }
 
-    static func remove(forKey: Keys) {
+    static func remove(forKey: Keys) -> Bool {
         UserDefaults.standard.removeObject(forKey: forKey.rawValue)
+        return UserDefaultsService.get(forKey: forKey) == nil
     }
 
     enum Keys: String {
